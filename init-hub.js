4022e693dee775c298f5b94ce70a5f9ee3e28569
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2020-08-12 23:51:58 +0200

    Add first stage of mongo db + mongo-express setup

---
+++ init-hub.js
@@ -0,0 +1,15 @@
+db.auth('root', 'password')
+
+db = db.getSiblingDB('tilt')
+
+db.createUser(
+    {
+    user: "tilt-user",
+    pwd: "tilt-password",
+    roles: [
+        {
+            role: "readWrite",
+            db: "tilt"
+        }
+    ]
+})
---
+++ sample-client.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient('mongodb://root:SuperSecret@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+filter={
+    'accessAndDataPortability.administrativeFee.currency': 'EUR'
+}
+
+result = client['tilt']['tilt'].find(
+  filter=filter
+)
+
+for document in result:
+    pprint(document)
