df5af1784d901ead117c08b2b6c8b69125e1ef87
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2020-08-17 01:06:05 +0200

    Update python trigger

--- triggers/python/main.py
+++ triggers/python/main.py
@@ -1,13 +1,20 @@
 from mongotriggers import MongoTrigger
 from pymongo import MongoClient
 
+from datetime import datetime
+
 from tilt import tilt
 
 def notify_manager(op_document):
-    print ('wake up! someone is adding me money')
+    print ('Document change occured!')
+    try:
+        t = tilt.from_dict(op_document)
+        print(tilt.meta.name)
+    except:
+        print('Could not create tilt object.')
 
 #client = MongoClient('mongodb://tilt-user:SuperSecret@mongo/?authSource=tilt&authMechanism=SCRAM-SHA-256')
-client = MongoClient('mongodb://root:SuperSecret@localhost/?authSource=admin&authMechanism=SCRAM-SHA-256&replicaSet=rs0')
+client = MongoClient('mongodb://root:SuperSecret@mongo/?authSource=admin&authMechanism=SCRAM-SHA-256&replicaSet=rs0')
 
 triggers = MongoTrigger(client)
 
@@ -20,7 +27,7 @@ triggers.register_insert_trigger(notify_manager, 'tilt', 'tilt')
 triggers.tail_oplog()
 
     # Action that should be performed
-client['tilt']['tilt'].insert_one({"validation_result": "completed"})
+client['tilt']['tilt-python-triggers'].insert_one({"validation" : datetime.timestamp(datetime.now()), "validation_result": "completed"})
 
 triggers.stop_tail()
 
