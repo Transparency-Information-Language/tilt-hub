b714493405afeb7e5eb14bc02173330e1033be61
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2020-08-17 01:05:50 +0200

    update sample client

--- sample-client.py
+++ sample-client.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 from pprint import pprint
 
 client = MongoClient('mongodb://root:SuperSecret@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+
+
 filter={
     'accessAndDataPortability.administrativeFee.currency': 'EUR'
 }
