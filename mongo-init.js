c71b76c8bc0ce8564035b1d45cdc22185d9df5a1
Elias Grünewald <gruenewald@campus.tu-berlin.de>
2021-03-19 00:56:40 +0100

    init

--- mongo-init.js
+++ mongo-init.js
@@ -25,4 +25,4 @@ db.createUser(
         }
 );
 
-rs.initiate({_id:"rs0", members: [{"_id": 0, "host": "127.0.0.1:27017"}]})
+//rs.initiate({_id:"rs0", members: [{"_id": 0, "host": "127.0.0.1:27017"}]})
